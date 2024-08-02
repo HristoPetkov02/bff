7655edccd66e6eb7da6a9f21a7fea80dd2d5cf19
LlamaLamap1 <hristo.petkov221@gmail.com>
2024-08-02 12:48:02 +0300

    The modules have been separated.

--- rest/src/main/java/com/tinqinacademy/bff/rest/BffApplication.java
+++ rest/src/main/java/com/tinqinacademy/bff/rest/BffApplication.java
@@ -1,9 +1,12 @@
-package com.tinqinacademy.bff.bff;
+package com.tinqinacademy.bff.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.tinqinacademy.bff")
 public class BffApplication {
 
 	public static void main(String[] args) {
